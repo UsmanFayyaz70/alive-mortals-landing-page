d8cf0ae4e4152fcfe62d99c41fa04917be8c2b08
Usman Fayyaz <usman.fayyaz@code-avenue.com>
2023-06-21 20:36:12 +0500

    *version updated

--- src/components/screen/index.js
+++ src/components/screen/index.js
@@ -1,24 +1,97 @@
 import React from 'react'
 import "./index.css"
+import { Button, Tooltip, message } from 'antd'
+import { BsTwitter, BsFillPersonFill } from 'react-icons/bs'
+import { RxDiscordLogo } from 'react-icons/rx'
+import { MdOutlineAlternateEmail } from 'react-icons/md'
+
 import { Alert, Col, Row } from 'antd'
 import Links from '../links'
 const Screen = () => {
-    const handleEmailClicked = () => {
-        <Alert message="SUCCESS" type='success' />
-    }
+    const [messageApi, contextHolder] = message.useMessage();
     return (
         <>
-            <link href='https://fonts.googleapis.com/css?family=Chewy' rel='stylesheet'></link>
+            <link
+                href='https://fonts.googleapis.com/css?family=Chewy'
+                rel='stylesheet'
+            ></link>
             <Row justify={'center'}>
-                <Col span={10} style={{ marginRight: "25px" }}>
-                    <h1 className='glow' style={{ marginTop: "20px" }}>
-                        Website Coming Soon...
-                    </h1>
+                <Col span={11}>
+                    <Row justify={'end'}>
+                        <Col span={19}>
+                            <h1
+                                className='glow'
+                                style={{ marginTop: "20px" }}
+                            >
+                                Website Coming Soon...
+                            </h1>
+                        </Col>
+                    </Row>
                 </Col>
-            </Row>
+            </Row >
 
             <Row style={{ margin: "0px 0px 0px 17px" }} gutter={[0, 0]}>
-                <Links />
+                {contextHolder}
+                <Col span={13}>
+                    <Tooltip placement='right' title="Twitter">
+                        <Button
+                            className='link-btn'
+                            type='link'
+                            href='https://twitter.com/MortalitiesClub'
+                            target='_blank'
+                            // style={{ height: "100px", width: "120px" }}
+                            size='medium'
+                            icon={<BsTwitter size={25} />}
+                            ghost
+                        />
+                    </Tooltip>
+                </Col>
+                <Col span={13} >
+                    <Tooltip placement='right' title="Discord">
+                        <Button
+                            className='link-btn'
+                            href='https://discord.gg/mD2wByEQ'
+                            target='_blank'
+                            type='link'
+                            size='medium'
+                            icon={<RxDiscordLogo size={25} />}
+                            ghost
+                        />
+                    </Tooltip>
+                </Col>
+                <Col span={13} >
+                    <Tooltip placement='right' title="Founder">
+                        <Button
+                            className='link-btn'
+                            type='link'
+                            href='https://twitter.com/Nawaf_Aslam'
+                            target='_blank'
+                            size='medium'
+                            icon={<BsFillPersonFill size={25} />}
+                            ghost
+                        />
+                    </Tooltip>
+                </Col>
+                <Col span={13} >
+                    <Tooltip placement='right' title="nft@alivemortals.club">
+                        <Button
+                            className='link-btn'
+                            type='link'
+                            size='medium'
+                            icon={<MdOutlineAlternateEmail size={25} />}
+                            ghost
+                            onClick={() => {
+                                navigator.clipboard.writeText("nft@alivemortals.club")
+                                messageApi.open({
+                                    content: 'Email has been copied to clipboard!',
+                                    duration: 2,
+                                    type: 'success'
+                                });
+                            }}
+                        // return alert("Email has been copied to clipboard")
+                        />
+                    </Tooltip>
+                </Col>
             </Row>
             <Row>
             </Row>
